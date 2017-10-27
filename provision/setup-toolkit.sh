38a6b4fdafe7965edcf84bf6eb00100d666780e2
ıqO <merenuou@gmail.com>
2017-10-27 12:54:51 -0600

    Allow errors and automate bare site import

--- provision/setup-toolkit.sh
+++ provision/setup-toolkit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # This file installs all needed plugins and theme(s)...and some dev dependencies.
 # Author : Obi Merenu
+set -e
 
 REPO_GH='git@github.com:'
 REPO_GH_HTTP='https://github.com'
@@ -9,6 +10,19 @@ WP_DIR='../public_html'
 PLUGIN_DIR='../public_html/wp-content/plugins'
 THEME_DIR='../public_html/wp-content/themes'
 WP_URL='https://downloads.wordpress.org/plugin'
+WP_CONF='../public_html/wp-config.php'
+
+if [ ! -e ${WP_CONF} ]
+then
+    echo "Wrong directory"
+else
+    # Get 'WordPress-Bare' project files
+    curl -LJO  ${REPO_GH_HTTP}'/Metumaribe/WordPress-Bare/archive/master.zip'
+    unzip *.zip
+    cp -a WordPress-Bare-master/. ${WP_DIR}
+    rm -rf WordPress-Bare-master
+    rm -rf *.zip
+fi
 
 # check if its the wordpress dir : @todo concatenate wp-config.php to $WP_DIR.
 if [ -d ${WP_DIR} ]; then
@@ -24,20 +38,19 @@ if [ -d ${WP_DIR} ]; then
     git clone ${REPO_GH}Metumaribe/easy-digital-downloads.git ${PLUGIN_DIR}/easy-digital-downloads
     git clone ${REPO_GH}Metumaribe/WordPress-Admin-Style.git ${PLUGIN_DIR}/WordPress-Admin-Style
     git clone ${REPO_GH}Metumaribe/wp-redis.git ${PLUGIN_DIR}/wp-redis
-     
+
 else
     echo "${PLUGIN_DIR} doesn't exists"
 fi
 
 cd ${WP_DIR}
 
-file_out=$(curl -o ./WordPress-Bare.zip  ${REPO_GH_HTTP}'/Metumaribe/WordPress-Bare/archive/master.zip' 2>&1)
+file_out=$(curl -o ./timber-library.zip ${WP_URL}'/timber-library.zip' 2>&1)
 
 if [[ $? -eq 0 && ${file_out} ]]; then
 
  # Install plugins from WordPress.org
  cd ${PLUGIN_DIR}
-
  # install debug files
  curl -o ./rewrite-rules-inspector.1.2.1.zip ${WP_URL}'/rewrite-rules-inspector.1.2.1.zip'
  curl -o ./wp-crontrol.1.5.zip ${WP_URL}'/wp-crontrol.1.5.zip'
@@ -46,12 +59,11 @@ if [[ $? -eq 0 && ${file_out} ]]; then
  curl -o ./query-monitor.2.15.0.zip ${WP_URL}'/query-monitor.2.15.0.zip'
  curl -o ./wordpress-mu-domain-mapping.0.5.5.1.zip ${WP_URL}'/wordpress-mu-domain-mapping.0.5.5.1.zip'
  curl -o ./multisite-clone-duplicator.1.4.1.zip ${WP_URL}'/multisite-clone-duplicator.1.4.1.zip'
- curl -o ./timber-library.zip ${WP_URL}'/timber-library.zip'
 
- # Unzip all files
- # unzip *.zip
+ # Unzip all files unzip *.zip
  find ./ -name \*.zip -exec unzip {} \;
  rm -rf *.zip
+ echo 'theses are your files.'
  ls -al
 
  rm -rf ${PLUGIN_DIR}/wp-crontrol/*
